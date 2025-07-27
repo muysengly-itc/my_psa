25569a559e167cd89e43d80a7e48e03def279481
MUY SENGLY <muy.sengly@itc.edu.kh>
2025-07-27 17:00:40 +0700

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 
 app = FastAPI()
@@ -21,9 +22,15 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/image")
+async def get_image():
+    return FileResponse("image/apple.png")
+
+
 if __name__ == "__main__":
     file_name = __file__.split("\\")[-1].split(".")[0]
-    uvicorn.run(f"{file_name}:app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run(f"{file_name}:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(f"{file_name}:app", host="0.0.0.0", port=8000, reload=True)
 
 
 # http://127.0.0.1:8000
